0d40991ae522276659e402a547a4eaf7861521e4
SavidDu <122414060+SavidDu@users.noreply.github.com>
2026-05-17 16:14:29 +1000

    Update grayscale.js

--- js/grayscale.js
+++ js/grayscale.js
@@ -5,13 +5,24 @@
  */
 
 // jQuery to collapse the navbar on scroll
-$(window).scroll(function() {
+// $(window).scroll(function() {
+//     if ($(window).scrollTop() > 50) {
+//         $(".navbar-fixed-top").addClass("top-nav-collapse");
+//     } else {
+//         $(".navbar-fixed-top").removeClass("top-nav-collapse");
+//     }
+// });
+
+function updateNavbar() {
     if ($(window).scrollTop() > 50) {
         $(".navbar-fixed-top").addClass("top-nav-collapse");
     } else {
         $(".navbar-fixed-top").removeClass("top-nav-collapse");
     }
-});
+}
+
+$(window).scroll(updateNavbar);
+$(document).ready(updateNavbar);
 
 // jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function() {
