380166253e163ff0a64d211a520a8bf6f6699a58
SavidDu <122414060+SavidDu@users.noreply.github.com>
2026-05-21 11:26:42 +1000

    Update grayscale.js

--- js/grayscale.js
+++ js/grayscale.js
@@ -13,14 +13,6 @@ $(window).scroll(function() {
     }
 });
 
-$(window).scroll(function() {
-    if ($(".navbar").offset().top > 50) {
-        $(".navbar-fixed-top").addClass("top-nav-collapse");
-    } else {
-        $(".navbar-fixed-top").removeClass("top-nav-collapse");
-    }
-});
-
 // jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function() {
     $('a.page-scroll').bind('click', function(event) {
